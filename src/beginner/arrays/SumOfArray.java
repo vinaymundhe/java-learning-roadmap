891aaea64278a237ded2ac4b033073861550a6df
Vinay Mundhe <vinay.mundhe19@gmail.com>
2024-11-25 22:36:38 +0530

    Arrays

---
+++ src/beginner/arrays/SumOfArray.java
@@ -0,0 +1,18 @@
+package beginner.arrays;
+
+import java.util.Arrays;
+
+// Sum of Array: Input an array and calculate its sum.
+public class SumOfArray {
+    public static void main(String[] args) {
+
+        int[] rollNo = {1, 2, 3, 4, 5, 6, 7};
+
+        int sum = 0;
+        for (int allRollNo : rollNo) {
+            sum = allRollNo + sum;
+//            sum = Arrays.stream(rollNo).sum();
+        }
+        System.out.println(sum);
+    }
+}
