60f62cba36502bf9940919d61e86826717223252
Vinay Mundhe <vinay.mundhe19@gmail.com>
2025-01-03 13:03:41 +0530

    Started withCollections programs

---
+++ src/beginner/collections/UniqueWordsCounter.java
@@ -0,0 +1,18 @@
+package beginner.collections;
+
+import java.util.*;
+
+/* Write a program that takes a sentence as input from the user, splits it into words, and then counts the unique words using a HashSet.*/
+public class UniqueWordsCounter {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter a sentence: ");
+        String userInput = scn.nextLine();
+        String [] inputInWords = userInput.split(" ");
+
+        Set<String> uniqueWords = new HashSet<>();
+        uniqueWords.addAll(Arrays.asList(inputInWords));
+        System.out.println("Unique Words: "+uniqueWords);
+        System.out.println("The number of unique words: "+ uniqueWords.size());
+    }
+}
