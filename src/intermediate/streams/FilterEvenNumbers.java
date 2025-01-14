2a7cdc00b64d4096d3c27d3fdee398b76c0627e0
Vinay Mundhe <vinay.mundhe19@gmail.com>
2025-01-14 20:04:10 +0530

    added basic streams problems

---
+++ src/intermediate/streams/FilterEvenNumbers.java
@@ -0,0 +1,17 @@
+package intermediate.streams;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// Given a list of integers, filter and print only the even numbers.
+public class FilterEvenNumbers {
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+        numbers.stream()
+                .filter(num -> num %2 == 0)
+                .forEach(System.out::println);
+    }
+}
---
+++ src/intermediate/streams/StringsToUppercase.java
@@ -0,0 +1,16 @@
+package intermediate.streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+// Given a list of strings, convert them to uppercase.
+public class StringsToUppercase {
+    public static void main(String[] args) {
+
+        List<String> names = Arrays.asList("John","Vinay","Anjali","Rasika");
+
+        names.stream()
+                .map(String::toUpperCase)
+                .forEach(System.out::println);
+    }
+}
