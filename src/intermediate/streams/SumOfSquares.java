08996607745429cf84530a1b3eb84f7a76d06c01
Vinay Mundhe <vinay.mundhe19@gmail.com>
2025-01-16 23:49:17 +0530

    Basic streams problems

---
+++ src/intermediate/streams/SumOfSquares.java
@@ -0,0 +1,17 @@
+package intermediate.streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+// Find the sum of squares of all numbers in a list.
+public class SumOfSquares {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+
+        int sumOfSquares = numbers.stream()
+                .map(num -> num * num)
+                .reduce(0, Integer::sum);
+
+        System.out.println("Sum of Squares: " + sumOfSquares); // Output: 55
+    }
+}
